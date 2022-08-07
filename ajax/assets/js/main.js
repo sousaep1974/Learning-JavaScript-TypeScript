d95ecd63212c2c511c5d95831dec58122cf65e33
Edivan Sousa <sousaep1974@gmail.com>
2022-08-07 00:00:25 -0300

    Learning: fetch-axios

--- ajax/assets/js/main.js
+++ ajax/assets/js/main.js
@@ -44,3 +44,12 @@ function carregaResultado(response) {
   const resultado = document.querySelector('.resultado');
   resultado.innerHTML = response;
 }
+
+
+fetch('pagina4.html')
+  .then(resposta => {
+    if (resposta.status !== 200) throw new Error('ERRO 404 NOSSO');
+    return resposta.text();
+  })
+  .then(html => console.log(html))
+  .catch(e => console.log(e));
---
+++ fetch-axios/assets/js/main.js
@@ -0,0 +1,35 @@
+/* fetch('pessoas.json')
+  .then(resposta => resposta.json())
+  .then(json => carregaElementosNaPagina(json)); */
+
+axios('pessoas.json')
+.then(resposta => carregaElementosNaPagina(resposta.data));
+
+function carregaElementosNaPagina(json) {
+  const table = document.createElement('table');
+
+  for(let pessoa of json) {
+    const tr = document.createElement('tr');
+
+    let td1 = document.createElement('td');
+    td1.innerHTML = pessoa.nome;
+    tr.appendChild(td1);
+
+    let td2 = document.createElement('td');
+    td2.innerHTML = pessoa.idade;
+    tr.appendChild(td2);
+    
+    let td3 = document.createElement('td');
+    td3.innerHTML = pessoa.email;
+    tr.appendChild(td3);
+    
+    let td4 = document.createElement('td');
+    td4.innerHTML = pessoa.salario;
+    tr.appendChild(td4);
+
+    table.appendChild(tr);
+  }
+
+  const resultado = document.querySelector('.resultado');
+  resultado.appendChild(table);
+}
