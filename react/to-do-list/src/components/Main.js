40342cb9f5b3ed674dc072494bb13dd23263300a
Edivan Sousa <sousaep1974@gmail.com>
2022-11-27 23:54:50 -0300

    React Learning (if, else, setState, preventDefault, trim(), splice())

--- react/to-do-list/src/components/Main.js
+++ react/to-do-list/src/components/Main.js
@@ -1,9 +1,43 @@
 import React, { Component } from 'react';
+
+//Form
+import { FaEdit, FaWindowClose } from 'react-icons/fa';
+
+//Tasks
+import { FaPlus } from 'react-icons/fa';
+
 import './Main.css';
 
 class Main extends Component {
   state = {
     taskNew: '',
+    tasks: [],
+    index: -1,
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { tasks, index } = this.state;
+    let { taskNew } = this.state;
+    taskNew = taskNew.trim();
+
+    if (tasks.indexOf(taskNew) !== -1) return;
+
+    const tasksNews = [...tasks];
+
+    if (index == -1) {
+      this.setState({
+        tasks: [...tasksNews, taskNew],
+        taskNew: '',
+      });
+    } else {
+      tasksNews[index] = taskNew;
+
+      this.setState({
+        tasks: [...tasksNews],
+        index: -1,
+      });
+    }
   };
 
   handleChange = (e) => {
@@ -11,16 +45,54 @@ class Main extends Component {
       taskNew: e.target.value,
     });
   };
+
+  handleEdit = (e, index) => {
+    const { tasks } = this.state;
+
+    this.setState({
+      index,
+      taskNew: tasks[index],
+    });
+  };
+
+  handleDelete = (e, index) => {
+    const { tasks } = this.state;
+    const tasksNews = [...tasks];
+    tasksNews.splice(index, 1);
+
+    this.setState({ tasks: [...tasksNews] });
+  };
+
   render() {
-    //const { taskNew } = this.state;
+    const { taskNew, tasks } = this.state;
     return (
       <>
         <div className="main">
           <h1>TO-DO LIST</h1>
-          <form action="#">
-            <input onChange={this.handleChange} type="text" />
-            <button type="submit">Send</button>
+          <form onSubmit={this.handleSubmit} action="#" className="form">
+            <input onChange={this.handleChange} type="text" value={taskNew} />
+            <button type="submit">
+              <FaPlus />
+            </button>
           </form>
+
+          <ul className="tasks">
+            {tasks.map((task, index) => (
+              <li key={task}>
+                {task}
+                <span>
+                  <FaEdit
+                    onClick={(e) => this.handleEdit(e, index)}
+                    className="edit"
+                  />
+                  <FaWindowClose
+                    onClick={(e) => this.handleDelete(e, index)}
+                    className="delete"
+                  />
+                </span>
+              </li>
+            ))}
+          </ul>
         </div>
       </>
     );
