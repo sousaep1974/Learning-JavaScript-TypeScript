9103940ecd59d301a9eb991a283d6c82daba3f5c
Edivan Sousa <sousaep1974@gmail.com>
2022-12-05 01:07:06 -0300

    React Learning (Styled-Components)

---
+++ react/react-base/src/pages/Login/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Title } from "./styled";
+
+function Login() {
+  return (
+    <>
+      <Title>
+        TESTE
+        <small>Oi</small>
+      </Title>
+      <p>Loren ipsum</p>
+    </>
+  );
+}
+
+export default Login;
